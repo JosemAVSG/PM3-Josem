9ea7c11dc34df3ba9e8834fa3ac588ba84c14599
Jose Miguel JGTECH <jmgg.95n@gmail.com>
2024-04-22 01:54:23 -0500

    adding cofig database and packages

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,13 @@
-import app from './server';
-import PORT  from './config/env';
+import app from "./server";
+import PORT from "./config/env";
+import "reflect-metadata";
+import { AppDataSource } from "./config/data-source";
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+AppDataSource.initialize()
+  .then((res) => {
+    console.log("Database connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(" Database connection error", error));
